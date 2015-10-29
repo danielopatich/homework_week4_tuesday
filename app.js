40ce49991a034ca1ed575e841a87595f0ded9e73
Daniel Opatich <daniel.opatich@gmail.com>
2015-10-29 14:26:11 -0500

    repos updated correctly

--- app.js
+++ app.js
@@ -11,49 +11,13 @@ var repoFeed = $('.repoLog');
 $.getJSON('https://api.github.com/users/danielopatich/repos').done(function(data){
  var displayRepos = function(repos) {
    repos.forEach(function(repo) {
-     console.log(repo);
-     var repo = $('<section class="repo"></section>');
-     var name = $('<a class="name" href="'+ repo.html_url +'">' + repo.name + '</a>');
 
-     repo.append(name);
-     repoFeed.append(repo);
+     var repoContainer = $('<section class="repo"></section>');
+     var nameContainer = $('<a class="name" href="'+ repo.html_url +'">' + repo.name + '</a>');
+
+     repoContainer.append(nameContainer);
+     repoFeed.prepend(repoContainer);
    });
  }
  displayRepos(data);
 });
-// fixed repos from going infinte, but no cant get them to line in a row //
-
-
-
-
-
-
-
-
-
-
-
-
-// var url = 'https://api.github.com/users/danielopatich/repos';
-//
-// var displayRepos = function(repos){
-//   log.html('');
-//
-//   repos.forEach(function(repo){
-//     var repoFeed = $('');
-//     var repoName = $('<a href="' + repoUrl + '">' + repoName + '</a>');
-//
-//     repoFeed.append(repoName);
-//     repoFeed.append(repoFeed);
-//
-//     log.append(repoName);
-//
-//   });
-// }
-//
-// var getRepos = function(){
-//   var success = function(data){
-//     displayRepos(data);
-//   }
-//   $.get('https://api.github.com/users/danielopatich/repos', null, handleSuccess);
-// };
