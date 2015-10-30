acdab1e16936ca52656078ac461c1287c5406d90
Daniel Opatich <daniel.opatich@gmail.com>
2015-10-29 22:43:39 -0500

    final commit, needs work

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ $.getJSON('https://api.github.com/users/danielopatich/repos').done(function(data
 
      var repoContainer = $('<section class="repo"></section>');
      var nameContainer = $('<a class="name" href="'+ repo.html_url +'">' + repo.name + '</a>');
-
+    //  var timeContainer = $('<a class="time" href="'+ repo.updated_at +'">' + repo.name + '</a>');
+     //
+    //  timeContainer.append(nameContainer)
      repoContainer.append(nameContainer);
      repoFeed.prepend(repoContainer);
    });
@@ -22,7 +24,19 @@ $.getJSON('https://api.github.com/users/danielopatich/repos').done(function(data
  displayRepos(data);
 });
 
-
+// $getJSON('https://api.github.com/users/danielopatich/repos').done(function(data){
+//   var then = $('then'),
+//       date = moment(updated_at(repo.attr('data-date'))),
+//
+//       var timeContainer = $('<a class="time" href="'+ repo.updated_at +'">' + repo.name + '</a>');
+//
+//       timeContainer.append(nameContainer)
+//
+//
+//       update = function(){
+//                  repo.updated_at(date.fromNow());
+//                };
+// });
 
 
 //
