2fbf94e33b55de28c2ec0543e18673970e356106
Daniel Opatich <daniel.opatich@gmail.com>
2015-11-01 22:54:25 -0600

    added profile bio

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ $.ajaxSetup({
 });
 
 
-var url = 'https://api.github.com/users/danielopatich/repos';
+var url = 'https://api.github.com/users/danielopatich';
 var repoFeed = $('.repoLog');
+var followFeed = $('#leftColumn')
 
 $.getJSON('https://api.github.com/users/danielopatich/repos').done(function(data){
  var displayRepos = function(repos) {
-   repos.forEach(function(repo) {
 
+   repos.forEach(function(repo) {
+     var time = moment(repo.updated_at).format('ll');
      var repoContainer = $('<section class="repo"></section>');
-     var nameContainer = $('<a class="name" href="'+ repo.html_url +'">' + repo.name + '</a>');
-    //  var timeContainer = $('<a class="time" href="'+ repo.updated_at +'">' + repo.name + '</a>');
-     //
-    //  timeContainer.append(nameContainer)
+     var nameContainer = $('<a class="name" href="'+ repo.html_url +'">' + repo.name + '</a><a class="time"> Updated At ' + time + '</a>');
+
      repoContainer.append(nameContainer);
      repoFeed.prepend(repoContainer);
    });
@@ -24,20 +24,19 @@ $.getJSON('https://api.github.com/users/danielopatich/repos').done(function(data
  displayRepos(data);
 });
 
-// $getJSON('https://api.github.com/users/danielopatich/repos').done(function(data){
-//   var then = $('then'),
-//       date = moment(updated_at(repo.attr('data-date'))),
-//
-//       var timeContainer = $('<a class="time" href="'+ repo.updated_at +'">' + repo.name + '</a>');
-//
-//       timeContainer.append(nameContainer)
-//
-//
-//       update = function(){
-//                  repo.updated_at(date.fromNow());
-//                };
-// });
 
 
-//
-// forEach(function (arr, index, array)
+$.getJSON('https://api.github.com/users/danielopatich').done(function(data){
+  console.log(data);
+    profileStats = data;
+
+    $(".profileBio").append("<li>" + profileStats.company + "</li>" )
+    $(".profileBio").append("<li>" + profileStats.location + "</li>" )
+    $(".profileBio").append("<li>" + profileStats.email + "</li>" )
+    $(".profileBio").append("<li>" + profileStats.blog + "</li>" )
+    $(".profileBio").append("<li>" + profileStats.created_at + "</li>" )
+
+    $(".followStats").append("<h2> Followers " + profileStats.followers + "</h2>" )
+    $(".followStats").append("<h2> Starred " + profileStats.followers + "</h2>" )
+    $(".followStats").append("<h2> Following " + profileStats.following + "</h2>" )
+});
